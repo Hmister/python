0a110daa91d8f13e82993581e09b150036e7d9bd
HMister <1196192447@qq.com>
2018-10-01 10:27:45 +0800

    oop笔记

---
+++ oop/01.py
@@ -0,0 +1,29 @@
+'''
+定义一个学生类,用来形容学生
+'''
+# 定义一个空的类
+class Student():
+    # 一个空类,pass代表直接跳过
+    # 此处pass 必须有
+    pass
+#定义一个对象
+mingyue = Student()
+# 再定义一个类，用来描述听Python的学生
+class PythonStudent():
+    #用None给不确定的值赋值
+    name = None
+    age = 8
+    course = "Python"
+    # 需要注意
+    # 1.def doHomework的缩进层级
+    # 2.系统默认出一个self参数
+    def doHomework(self):
+        print("I 在做作业")
+        #在函数末尾使用return 语句
+        return None
+# 实例化一个叫yueyue的学生，是一个具体的人
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+# 注意成员函数的调用没有传递竟如参数
+yueyue.doHomework()
